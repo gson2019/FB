63a6b72599849070e612d0ef5d64a99e28b1dc59
Rui Liu <rliu755@gmail.com>
2020-02-18 20:43:18 -0600

    Initial commit

---
+++ src/Leet973.java
@@ -0,0 +1,48 @@
+public class Leet973 {
+    public int[][]  kikClosest(int[][] points, int K) {
+        int start=0, end=points.length-1;
+        while(start < end) {
+            int pivot = partition(points, start, end);
+            if(pivot > (K-1)) {
+                end = pivot-1;
+            }
+            else if(pivot < (K-1)) {
+                start = pivot+1;
+            }
+            else {
+                break; //found the Kth largest index
+            }
+        }
+
+        int[][] result = new int[K][2];
+        for(int i=0; i < K; i++) {
+            result[i] = new int[]{points[i][0], points[i][1]};
+        }
+        return result;
+    }
+
+    private int partition(int[][] points, int start, int end) {
+        int pivot = start;
+        while(start <= end) {
+            while(start <= end && calcDist(points[start]) <= calcDist(points[pivot])) start++;
+            while(start <= end && calcDist(points[end]) > calcDist(points[pivot])) end--;
+            if(start <= end) {
+                swap(points, start, end);
+                start++;
+                end--;
+            }
+        }
+        swap(points, end, pivot);
+        return end;
+    }
+
+    private void swap(int[][] points, int i, int j) {
+        int[] temp = points[j];
+        points[j] = points[i];
+        points[i] = temp;
+    }
+
+    private int calcDist(int[] point) {
+        return point[0]*point[0] + point[1]*point[1];
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+//        Leet301 lc301 = new Leet301();
+//        String input = "(a)())()";
+//        List<String> res = lc301.removeInvalidParentheses(input);
+
+        Leet973 lc973 = new Leet973();
+        int[][] points = new int[][]{
+                {-2, 5},
+                {-1, 3},
+                {1, 1},
+                {2, 1},
+                {3, 2},
+                {1, -3},
+                {2, -3},
+                {3, -1},
+                {6, -3}
+        };
+        int[][] res = lc973.kClosest(points, 6);
+
+//        Leet215 leet215 = new Leet215();
+//        int[] nums = new int[]{5, 4, 8, 6, 9, 5, 7};
+        // 找到第五大的数字，
+        // 4 => 5 => 5 => 6 => 7 => 8 => 9
+//        int kth = leet215.findKthLargest(nums, 3);
+    }
+}
